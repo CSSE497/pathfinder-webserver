c258ade1a4faeb2cb7b9ba58754d858d7c322344
Adam Michael <adam@ajmichael.net>
2016-01-20 01:30:17 -0500

    Created cluster explorer panel.
    Maps don't display commodities and vehicles yet. They will soon.

--- public/js/application.js
+++ public/js/application.js
@@ -1,34 +1,69 @@
 $(function() {
-    function getTree() {
-        return [{
-            text: "application id",
-            nodes: [
-                {
-                    text: "USA",
-                    nodes: [
-                        {
-                            text: "NYC",
-                        },
-                        {
-                            text: "SF"
-                        }
-                    ]
-                },
-                {
-                    text: "UK",
-                    nodes: [
-                        {
-                            text: "London"
-                        }
-                    ]
-                }
-            ]
-        }]
+
+    if (!Array.prototype.last) {
+        Array.prototype.last = function() {
+            return this[this.length - 1];
+        };
+    };
+
+    function tree(cluster) {
+        return {
+            text: cluster.id.split("/").last(),
+            state: { expanded: true },
+            nodes: cluster.subclusters.map(tree)
+        };
     }
-    console.log("Made it this far");
-    $('#clustertree').treeview({
-        data: getTree(),
-        showBorder: false,
+
+    function subclusterId(dataNodeId) {
+        var node = $("#clustertree").treeview("getNode", dataNodeId);
+        if (node.parentId != undefined) {
+            return subclusterId(node.parentId) + "/" + node.text;
+        } else {
+            return node.text;
+        }
+    }
+
+    function currentSubclusterId() {
+        var selected = $("#clustertree").treeview('getSelected')[0];
+        return subclusterId(selected.nodeId);
+    }
+
+    function createSubcluster() {
+        var name = $("#subclusterinput").val();
+        var parent = currentSubclusterId();
+        var path = parent + "/" + name;
+        pf.createCluster(path, function(cluster) {
+            loadTree();
+        });
+    }
+
+    function updateMap(path) {
+        $("#maplabel").text("Cluster: " + path);
+        pf.getCluster(path, function(cluster) {
+            console.log("Attempting to create map for");
+            console.log(cluster);
+
+
+        });
+    }
+
+    function loadTree() {
+        pf.getDefaultCluster(function(cluster) {
+            $('#clustertree').treeview({
+                data: [tree(cluster)],
+                showBorder: false,
+                onNodeSelected: function(event, data) {
+                    updateMap(currentSubclusterId());
+                }
+            });
+        });
+    }
+
+    loadTree();
+    $("#createsubcluster").click(createSubcluster);
+    $("#createsubcluster").prop("disabled", true);
+    $("#subclusterinput").keyup(function() {
+        $("#createsubcluster").prop("disabled", this.value == "" ? true : false);
     });
 });
 
